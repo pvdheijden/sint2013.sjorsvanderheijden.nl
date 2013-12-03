9e02780432b467a1d95032219d2885ec550fcf7b
Pim van der Heijden <pim@zifzaf.com>
2013-12-03 18:28:35 +0100

    first working version

--- web.js
+++ web.js
@@ -23,7 +23,7 @@ var questions = [
 {
   title: "Ken je zelf!",
   subtitle: " ... of hoe andere jou denken te kennen",
-  imageURL: "/photo-1.jpg",
+  imageURL: "/1-sjors.jpg",
   question: "Wie staat hier op de foto?",
   answerKeywords: [
     { tag: "sjors" }
@@ -32,15 +32,86 @@ var questions = [
   nextURL: "/questions/1"
 },
 {
-  title: "XXX Ken je zelf!",
-  subtitle: " ... of hoe andere jou denken te kennen",
-  imageURL: "/photo-1.jpg",
-  question: "Wie staat hier op de foto?",
+  title: "Hollandse kost",
+  subtitle: "gewoon een karbonade, aardappels en spercieboontjes",
+  imageURL: "/2-stoofvlees.jpg",
+  question: "Is Sjors soms een boer, wat hij niet kent ...",
   answerKeywords: [
-    { tag: "sjors" }
+    { tag: "eet hij niet" }
   ],
-  hint: "Deze is te makkelijk, voor deze is geen hint beschikbaar!",
+  hint: "Kan me niet voorstellen dat je deze uitdrukking nog nooit hebt gehoord!",
   nextURL: "/questions/2"
+},
+{
+  title: "Tussen de middag, wat anders",
+  subtitle: "... heeeel graag zelfs",
+  imageURL: "/3-tosti.jpg",
+  question: "Wat eet Sjors ALTIJD tussen de middag?",
+  answerKeywords: [
+    { tag: "kaas tosti" },
+    { tag: "kaastosti" },
+    { tag: "tosti zonder ham" }
+  ],
+  hint: "De standaard Sjors lunch is geen gewone tosti maar ...",
+  nextURL: "/questions/3"
+},
+{
+  title: "Moet erbij",
+  subtitle: "... anders is geen enkele maaltijd compleet",
+  imageURL: "/4-ketchup.jpg",
+  question: "Zoals hier boven al staat, geen maaltijd is compleet zonder dit!",
+  answerKeywords: [
+    { tag: "heinz tomatenketchup" }
+  ],
+  hint: "Er is er maar een de enige echte!",
+  nextURL: "/questions/4"
+},
+{
+  title: "Eten, verteren, naar de WC en dan",
+  subtitle: "Je zou denken dat hij dat toch wel weet",
+  imageURL: "/5-wc.jpg",
+  question: "Na naar de WC te zijn geweest moet je ook ... en ...",
+  answerKeywords: [
+    { tag: "billen afvegen en doortrekken" }
+  ],
+  hint: "Niet te geloven dat je hier een hint voor nodig hebt :-)",
+  nextURL: "/questions/5"
+},
+{
+  title: "De piano",
+  subtitle: "wanneer wordt het concert gegeven?",
+  imageURL: "/6-piano.jpg",
+  question: "Elke week gaar Sjors naar de pianoles, maar dan moet je wel elke dag ...",
+  answerKeywords: [
+    { tag: "oefenen" }
+  ],
+  hint: "Dat je dit niet weet is dus het probleem, oefenen dus!",
+  nextURL: "/questions/6"
+},
+{
+  title: "Computer spelletjes",
+  subtitle: "... niet alleen op de computer, maar ook PS3, Wii en de X-Box",
+  imageURL: "/7-minecraft.png",
+  question: "Hebben we hier te maken met een probleem?",
+  answerKeywords: [
+    { tag: "verslaving" },
+    { tag: "verslaafd" }
+  ],
+  hint: "Iets waar je niet zonder kunt is een ...?",
+  nextURL: "/questions/7"
+},
+{
+  title: "YouTube filmpjes",
+  subtitle: "niet alleen kijken",
+  imageURL: "/8-youtube.png",
+  question: "Er worden wel heeeeel veel filmpjes bekeken, maar gelukkig worden er ook filmpjes ...",
+  answerKeywords: [
+    { tag: "maken" },
+    { tag: "gemaakt" },
+    { tag: "upload"}
+  ],
+  hint: "kaaskipcraft van der heijden - als eerste in de familie - heeft het 4 keer gedaan",
+  nextURL: "/questions/7"
 }
 ];
 
